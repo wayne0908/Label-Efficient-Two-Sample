252fcb62799bc67ee16c704ef92de008f5129dd0
Weizhi Li <weizhi0908@gmail.com>
2021-11-23 20:49:49 -0700

    Add files via upload

--- Run.sh
+++ Run.sh
@@ -1,8 +1,14 @@
 # Passive, Bimodal, Certainty, Uncertainty, 
 for t in {1..200}
 do 	
-	python main.py --DataType='Syn' --Trial=$t --PermuTrial=500  --FeatLen=2 --Sep=1 --Del=0.6 --S=2000 --InitSize=50 --qs='Passive' --TestType='Chen' --alpha=0.05 --ChenM=1 --Interval=0.1 --Per=1 --cls='logistic' --RunTest=1 --LoadQuery=0 --Plot_Stats=1 --Plot_FD2=0 --Plot_Trend=0 	
-	python main.py --DataType='Syn' --Trial=$t --PermuTrial=500  --FeatLen=2 --Sep=1 --Del=0.6 --S=2000 --InitSize=50 --qs='Bimodal' --TestType='Chen' --alpha=0.05 --ChenM=1 --Interval=0.1 --Per=1 --cls='logistic' --RunTest=1 --LoadQuery=0 --Plot_Stats=1 --Plot_FD2=0 --Plot_Trend=0
-	python main.py --DataType='Syn' --Trial=$t --PermuTrial=500  --FeatLen=2 --Sep=1 --Del=0.6 --S=2000 --InitSize=50 --qs='Certainty' --TestType='Chen' --alpha=0.05 --ChenM=1 --Interval=0.1 --Per=1 --cls='logistic' --RunTest=1 --LoadQuery=0 --Plot_Stats=1 --Plot_FD2=0 --Plot_Trend=0
-	python main.py --DataType='Syn' --Trial=$t --PermuTrial=500  --FeatLen=2 --Sep=1 --Del=0.6 --S=2000 --InitSize=50 --qs='Uncertainty' --TestType='Chen' --alpha=0.05 --ChenM=1 --Interval=0.1 --Per=1 --cls='logistic' --RunTest=1 --LoadQuery=0 --Plot_Stats=1 --Plot_FD2=0 --Plot_Trend=0
+	python main.py --DataType='Syn' --Trial=$t --PermuTrial=500  --FeatLen=2 --Sep=1 --Del=0.6 --S=2000 --InitSize=50 --qs='Passive' --TestType='Chen' --alpha=0.05 --ChenM=1 --Interval=0.1 --Per=1 --cls='logistic' --RunTest=1 --LoadQuery=0 --Plot_Stats=1 --Plot_FD=0 --Plot_FD2=1 --Plot_Trend=0 	
+	python main.py --DataType='Syn' --Trial=$t --PermuTrial=500  --FeatLen=2 --Sep=1 --Del=0.6 --S=2000 --InitSize=50 --qs='Bimodal' --TestType='Chen' --alpha=0.05 --ChenM=1 --Interval=0.1 --Per=1 --cls='logistic' --RunTest=1 --LoadQuery=0 --Plot_Stats=1 --Plot_FD=0 --Plot_FD2=1 --Plot_Trend=0
+	python main.py --DataType='Syn' --Trial=$t --PermuTrial=500  --FeatLen=2 --Sep=1 --Del=0.6 --S=2000 --InitSize=50 --qs='Certainty' --TestType='Chen' --alpha=0.05 --ChenM=1 --Interval=0.1 --Per=1 --cls='logistic' --RunTest=1 --LoadQuery=0 --Plot_Stats=1 --Plot_FD=0 --Plot_FD2=1 --Plot_Trend=0
+	python main.py --DataType='Syn' --Trial=$t --PermuTrial=500  --FeatLen=2 --Sep=1 --Del=0.6 --S=2000 --InitSize=50 --qs='Uncertainty' --TestType='Chen' --alpha=0.05 --ChenM=1 --Interval=0.1 --Per=1 --cls='logistic' --RunTest=1 --LoadQuery=0 --Plot_Stats=1 --Plot_FD=0 --Plot_FD2=1 --Plot_Trend=0
+done
+
+# Passive, Bimodal, Certainty, Uncertainty, 
+for t in {200..200}
+do 	
+	python main.py --DataType='Syn' --Trial=$t --PermuTrial=500  --FeatLen=2 --Sep=1 --Del=0.6 --S=2000 --InitSize=50 --qs='Passive' --TestType='Chen' --alpha=0.05 --ChenM=1 --Interval=0.1 --Per=1 --cls='logistic' --RunTest=0 --LoadQuery=0 --Plot_Stats=1 --Plot_FD=1 --Plot_Trend=1 	
 done
